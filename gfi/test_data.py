7b8265a5ab7e72d20480a031dee81a1c9b38022d
RJ722 <rahul722j@gmail.com>
2020-02-14 19:32:07 +0530

    Fail CI if a duplicate entry is found

--- gfi/test_data.py
+++ gfi/test_data.py
@@ -7,6 +7,11 @@ import toml
 DATA_FILE_PATH = 'data/repositories.toml'
 
 
+def _get_data_from_toml(file_path):
+    with open(file_path, 'r') as file_desc:
+        return toml.load(file_desc)
+
+
 class TestDataSanity(unittest.TestCase):
     """Test for sanity of the data file."""
 
@@ -18,9 +23,15 @@ class TestDataSanity(unittest.TestCase):
     @staticmethod
     def test_data_file_sane():
         """Verify that the file is a valid TOML with required data."""
-        with open(DATA_FILE_PATH, 'r') as file_desc:
-            data = toml.load(file_desc)
+        data = _get_data_from_toml(DATA_FILE_PATH)
         assert 'repositories' in data
+
+    @staticmethod
+    def test_no_duplicates():
+        """Verify that all entries are unique."""
+        data = _get_data_from_toml(DATA_FILE_PATH)
+        repos = data.get('repositories', [])
+        assert len(repos) == len(set(repos))
 
 
 if __name__ == '__main__':
