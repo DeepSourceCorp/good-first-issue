959c9a202ab7a95724976ac6c3fd7075bca92245
pierreyoda <pierreyoda@users.noreply.github.com>
2021-01-23 13:16:50 +0100

    Add issues comments counter next to issue #number

--- gfi/populate.py
+++ gfi/populate.py
@@ -114,6 +114,7 @@ def get_repository_info(owner, name):
                         "title": issue.title,
                         "url": issue.html_url,
                         "number": issue.number,
+                        "comments_count": issue.comments_count,
                         "created_at": issue.created_at.isoformat(),
                     }
                 )
