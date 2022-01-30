0bd62bca9dfec157f4cd1bcf7f7fec02de5ec211
Zachary Shenkman <yankeedude02@gmail.com>
2022-01-30 17:14:35 -0500

    Resolves formatting errors in sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,17 +1,14 @@
-// THIS FILE SHOULD NOT BE VERSION CONTROLLED
-
-// https://github.com/NekR/self-destroying-sw
-
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function () {
   self.skipWaiting()
 })
 
-self.addEventListener('activate', function (e) {
-  self.registration.unregister()
+self.addEventListener('activate', function () {
+  self.registration
+    .unregister()
     .then(function () {
       return self.clients.matchAll()
     })
     .then(function (clients) {
-      clients.forEach(client => client.navigate(client.url))
+      clients.forEach((client) => client.navigate(client.url))
     })
 })
