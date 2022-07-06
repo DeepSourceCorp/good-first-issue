ea031dd051d2e7e4a8ac332eee85c8c23a3ce277
sandeep <venkatas@geekyants.com>
2022-07-06 11:23:03 +0530

    Moved repo mutations to a separate file

--- store/index.js
+++ store/index.js
@@ -1,3 +1,4 @@
+import { repoMutations } from './mutations/repo'
 import { sortingMutations } from './mutations/sorting'
 
 export const state = () => ({
@@ -7,12 +8,6 @@ export const state = () => ({
 })
 
 export const mutations = {
-  expandIssue(state, id) {
-    if (state.activeIssue === id.toString()) {
-      state.activeIssue = ''
-    } else {
-      state.activeIssue = id.toString()
-    }
-  },
+  ...repoMutations,
   ...sortingMutations
 }
