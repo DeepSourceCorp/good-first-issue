0803beadddc210eecb27ef7cf049b8fc3ae03055
sandeep <venkatas@geekyants.com>
2022-07-07 21:14:22 +0530

    Minor fix to handleActiveSortOptionToggle mutation

--- store/mutations/sorting.js
+++ store/mutations/sorting.js
@@ -1,6 +1,6 @@
 export const sortingMutations = {
   handleActiveSortOptionToggle(state, sortOption) {
-    if (state.activeSortOption === sortOption) {
+    if (JSON.stringify(state.activeSortOption) === JSON.stringify(sortOption)) {
       state.activeSortOption = {}
     } else {
       state.activeSortOption = sortOption
