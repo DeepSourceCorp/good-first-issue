e911ddbe7a6f439deba66132027a716405c6a6b6
Jai Pradeesh <68369919+jai-deepsource@users.noreply.github.com>
2022-10-10 13:10:46 +0530

    Remove banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="bg-ink-400 flex flex-col min-h-screen antialiased text-vanilla-300">
-    <banner>
-      <a
-        :href="BANNER.CTA.LINK"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="flex flex-row items-center text-center justify-center space-x-2"
-      >
-        <span>{{ BANNER.TEXT }}</span>
-        <ExternalLinkIcon class="md:inline-block hidden" size="1x" />
-      </a>
-    </banner>
     <navbar :tag="tag"></navbar>
     <main class="flex flex-1">
       <section class="container max-w-6xl mx-auto flex flex-col md:flex-row">
@@ -24,27 +13,17 @@
 <script>
 import Navbar from '~/components/Navbar.vue'
 import Sidebar from '~/components/Sidebar.vue'
-import Banner from '~/components/Banner.vue'
 import { ExternalLinkIcon } from 'vue-feather-icons'
-
-const BANNER = {
-  TEXT: 'Fix code quality issues in 10,000+ open-source projects with DeepSource Discover',
-  CTA: {
-    LINK: 'https://deepsource.io/discover'
-  }
-}
 
 export default {
   components: {
     Navbar,
     Sidebar,
-    Banner,
     ExternalLinkIcon
   },
   data: function () {
     return {
-      tag: {},
-      BANNER
+      tag: {}
     }
   }
 }
