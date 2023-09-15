71e7533a63a110bdd5224546a1c7c3e13bb40b71
Shubhankar Valimbe <59073366+shubhankarval@users.noreply.github.com>
2023-09-15 17:42:25 -0500

    Change content 's initial zoom
    Signed-off-by: Shubhankar Valimbe <59073366+shubhankarval@users.noreply.github.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
     ],
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, minimum-scale=1.0, maximum-scale=1.0' },
       {
         hid: 'description',
         name: 'description',
