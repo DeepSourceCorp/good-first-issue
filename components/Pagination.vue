c0348e4676467cf4dd61a171b6ec3668c62ff39d
Siddharth Dagar <sid.dagar0101@gmail.com>
2024-02-28 12:51:30 +0530

    Add Pagination feature

---
+++ components/Pagination.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex justify-center items-center mt-4">
+    <button
+      class="px-4 py-2 rounded text-ink-400 font-bold bg-juniper hover:bg-light_juniper"
+      @click="goToPrevPage"
+      :disabled="currentPage <= 1"
+    >
+      Previous
+    </button>
+
+    <span class="mx-4 font-bold">Page {{ currentPage }} of {{ totalPages }}</span>
+
+    <button
+      class="px-4 py-2 rounded text-ink-400 font-bold bg-juniper hover:bg-light_juniper"
+      @click="goToNextPage"
+      :disabled="currentPage >= totalPages"
+    >
+      Next
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  currentPage: Number,
+  totalPages: Number
+});
+
+const emit = defineEmits(['update:currentPage']);
+
+function goToPrevPage() {
+  if (props.currentPage > 1) {
+    emit('update:currentPage', props.currentPage - 1);
+  }
+}
+
+function goToNextPage() {
+  if (props.currentPage < props.totalPages) {
+    emit('update:currentPage', props.currentPage + 1);
+  }
+}
+</script>
