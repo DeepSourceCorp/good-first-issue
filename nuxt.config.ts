82c3ec669a6ebc50c881072d500871fae616fa86
Santiago <santiagocarvajal46@gmail.com>
2026-05-23 18:01:27 -0500

    chore: add Nuxt compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2026-05-23',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-gtag', 'nuxt-simple-sitemap'],
   nitro: {
